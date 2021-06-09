a51b64675e76ae083f8963e9ce378fada262249e
Daniel Cutter <dan.cutter@hotmail.co.uk>
2021-06-09 22:37:08 +0100

    basic api and create-react-app

--- babonApp/views.py
+++ babonApp/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
-
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
 
 def index(request):
     return HttpResponse("Hello, world. You're at the babonApp index.")
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
